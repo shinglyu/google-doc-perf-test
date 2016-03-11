18e8465b273b88c72781b12684f270e7c23c70fb
Shing Lyu <shing.lyu@gmail.com>
2016-03-11 17:19:12 +0800

    Change the logo's color for video start time matching

--- testperf.py
+++ testperf.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 import json
 
 class GoogleDocPerfTest(unittest.TestCase):
+    _profile_name = "test"
 
     def setUp(self):
         self.docUrl = "https://docs.google.com/document/d/1HXDdOQuxQiX1bSrpAs4hJfTaim1iLO_xB01nokb0wT0/edit?usp=sharing"
@@ -10,12 +11,17 @@ class GoogleDocPerfTest(unittest.TestCase):
         self.driver.get(self.docUrl)
 
     def test_load(self):
+
+        # Recording start marker
+        self.driver.execute_script("var teststart = function(){document.getElementById('docs-branding-logo').style.backgroundColor = 'red'}; teststart()");
+
         timings = self.driver.execute_script("return window.performance.timing")
         print(json.dumps(timings, indent=2))
         assert(True)
 
     def tearDown(self):
-        self.driver.close()
+        print('stopping')
+        #self.driver.close()
 
 if __name__ == "__main__":
     unittest.main()
